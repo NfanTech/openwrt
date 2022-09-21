e4c6c50f8d5c36a368be9559ae17e37f97aaf296
takeru94 <konoe2011@gmail.com>
2022-09-21 20:41:48 +0800

    Update restart-dns.sh

--- package/LeanMod/luci-app-autoreboot/root/etc/pure-dns/restart-dns.sh
+++ package/LeanMod/luci-app-autoreboot/root/etc/pure-dns/restart-dns.sh
@@ -1,4 +1,4 @@
 killall "/opt/pure-dns_linux_amd64"  2>/dev/null
-rm -rf /var/pdnsd/pdnsd.cache
+pdnsd-ctl -c /var/pdnsd empty-cache 2>/dev/null
 sleep 1s
 "/opt/pure-dns_linux_amd64" 2> /dev/null &
